0d697d030c8e4b8fe2be9552dc51b2577367b0fd
J.R. Ruggiero <fleury14@comcast.net>
2017-10-22 10:07:59 -0700

    added base for admin page, will set up child individual components and a child router next

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCa8nJqzKeh8GrXU8FsQ5-luekbWZaWeCg',
+    authDomain: 'fgc-ladder-1261d.firebaseapp.com',
+    databaseURL: 'https://fgc-ladder-1261d.firebaseio.com',
+    projectId: 'fgc-ladder-1261d',
+    storageBucket: 'fgc-ladder-1261d.appspot.com',
+    messagingSenderId: '216479046811'
+  }
 };
