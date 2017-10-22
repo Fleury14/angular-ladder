d04287d1a4fae1734717a47df4d0f3cec7c28169
J.R. Ruggiero <fleury14@comcast.net>
2017-10-22 10:22:31 -0700

    created base files for admin sub-components

---
+++ src/app/sub-pages/admin/player-management/player-management.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-admin-players',
+    templateUrl: './player-management.component.html',
+    styleUrls: [ './player-management.component.css']
+})
+
+export class PlayerManagementComponent {}
