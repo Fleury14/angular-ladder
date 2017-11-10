a1cc8ce674f91f24446189742c7834c698d9f342
J.R. Ruggiero <fleury14@comcast.net>
2017-11-10 11:17:20 -0800

    formatted date to be sent to server, form submits

--- src/app/sub-pages/admin/news-management/add/add-news.component.ts
+++ src/app/sub-pages/admin/news-management/add/add-news.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
     selector: 'app-admin-add-news',
@@ -8,8 +9,24 @@ import { Component } from '@angular/core';
 
 export class AddNewsComponent {
     public todaysDate: Date;
+    private _daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    private _monthsOfYear = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
+    'Septmeber', 'October', 'November', 'December'];
 
     constructor() {
         this.todaysDate = new Date();
+    }
+
+    public submitForm(form) {
+
+        const niceDate = this._daysOfWeek[this.todaysDate.getDay()] + ', ' + this._monthsOfYear[this.todaysDate.getMonth()] + ' '
+        + this.todaysDate.getDate() + ' ' + this.todaysDate.getFullYear();
+
+        const newItem = {
+            author: form.author,
+            date: niceDate,
+            content: form.content
+        };
+        console.log(newItem);
     }
 }
--- src/app/sub-pages/admin/news-management/news-management.module.ts
+++ src/app/sub-pages/admin/news-management/news-management.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewsRoutingModule, NewsRoutingComponents } from './news-management.routing.module';
+import { FormsModule } from '@angular/forms';
 
 import { NewsDatabaseService } from './../../../services/database/news-databse.service';
 
 @NgModule({
     declarations: [ NewsRoutingComponents ],
-    imports: [ CommonModule, NewsRoutingModule ],
+    imports: [ CommonModule, NewsRoutingModule, FormsModule ],
     providers: [ NewsDatabaseService ]
 })
 
