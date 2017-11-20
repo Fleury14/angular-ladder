debc0d8ee5c2708d3478ed43814dbdc3536db765
J.R. Ruggiero <fleury14@comcast.net>
2017-11-20 12:23:21 -0800

    sloppy workaround, but its a work around

--- src/app/sub-pages/admin/news-management/view/view-news.component.ts
+++ src/app/sub-pages/admin/news-management/view/view-news.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { NewsDatabaseService } from './../../../../services/database/news-databse.service';
 import NewsItem from './../../../../interfaces/news-item';
@@ -16,50 +17,40 @@ export class ViewNewsComponent implements OnInit {
 
     public newsList: NewsItem[];
     public secondList: NewsItem[];
+    public thirdList: NewsItem[];
     private moreArr;
 
-    constructor( private _newsService: NewsDatabaseService ) {
+    constructor( private _newsService: NewsDatabaseService, private _actroute: ActivatedRoute ) {
+
     }
 
     ngOnInit() {
 
-        this._newsService.moretest.subscribe(data => {
-            console.log('.moretest:', data);
-            this.secondList = [];
-            this.moreArr = data;
-            for (let item in data) {
-                const itemKey = item;
-                // console.log('inside loop', item);
-                // console.log('content too? please?', this.moreArr[item]);
-                this.moreArr[item].id = item;
-                const soCloseItem = {
-                    id: item,
-                    date: this.moreArr[item].date,
-                    content: this.moreArr[item].content,
-                    author: this.moreArr[item].author
-                };
-                this.secondList.push(soCloseItem);
-            }
-            console.log('endresult omg did we do it inside the component??', this.secondList);
+        this._actroute.paramMap.subscribe(val => {
+            this._newsService.moretest.subscribe(data => {
+                console.log('moved to param subscription', data);
+                this.secondList = [];
+                this.moreArr = data;
+                for (let item in data) {
+                    const itemKey = item;
+                    // console.log('inside loop', item);
+                    // console.log('content too? please?', this.moreArr[item]);
+                    this.moreArr[item].id = item;
+                    const soCloseItem = {
+                        id: item,
+                        date: this.moreArr[item].date,
+                        content: this.moreArr[item].content,
+                        author: this.moreArr[item].author
+                    };
+                    this.secondList.push(soCloseItem);
+                }
+                console.log('endresult omg did we do it inside the component??', this.secondList);
+            });
+
+            console.log('view news component...', this._newsService.secondArr);
+            this.thirdList = this._newsService.secondArr;
         });
 
-        console.log('view news component...', this._newsService.secondArr);
-        // this._newsService.newsObservable
-        // .map(newslist => {
-        //     return newslist.map(newsItem => {
-        //         console.log('maybe', newslist.indexOf(newsItem), this._newsService.keylist);
-        //         const myNewsItem: NewsItem = {
-        //             author: newsItem.author,
-        //             date: newsItem.date,
-        //             content: newsItem.content,
-        //             id: this._newsService.keylist[newslist.indexOf(newsItem)]
-        //         };
-        //         return myNewsItem;
-        //     });
-        // })
-        // .subscribe(news => {
-        //     this.newsList = news;
-        // });
 
     } // end oninit
 
