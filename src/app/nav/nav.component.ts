508be1eaeec20ef4fdad12e5932842d7162ca63c
J.R. Ruggiero <fleury14@comcast.net>
2017-12-22 12:07:10 -0800

    recommented out login info button, alsoworked around admin list being private

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -13,7 +13,12 @@ export class NavComponent {
     private _adminList = ['114390552657346311906'];
     // in order: jr, rado
     private _adminUidList = ['mkomTbU76VUR5tIoLmmyP1luR5q1', 'Gxr9qQ1pczOCLyncfhLiLdhvhy32'];
+
     constructor(public login: LoginService) {
+    }
+
+    public adminCheck(uid: string) {
+        return this._adminUidList.includes(uid);
     }
 
 }
