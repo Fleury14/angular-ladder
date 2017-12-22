e8020f0996395ba6731c78b8defcc0820479fa86
J.R. Ruggiero <fleury14@comcast.net>
2017-12-22 12:00:16 -0800

    made nav component search through an array of admins instead of just looking for me

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -10,6 +10,10 @@ import { LoginService } from './../services/login.service';
 
 export class NavComponent {
 
-    constructor(public login: LoginService) {}
+    private _adminList = ['114390552657346311906'];
+    // in order: jr, rado
+    private _adminUidList = ['mkomTbU76VUR5tIoLmmyP1luR5q1', 'Gxr9qQ1pczOCLyncfhLiLdhvhy32'];
+    constructor(public login: LoginService) {
+    }
 
 }
