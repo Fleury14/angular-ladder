dca26e42ea5e9b2b5fc9565c344377c7a7ae4e9e
J.R. Ruggiero <fleury14@comcast.net>
2017-12-22 12:40:25 -0800

    created initial ladder database service, for now the goal is to instantiate the current list just to have something to work with

---
+++ src/app/services/database/ladder-database.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { AngularFireDatabase } from 'angularfire2/database';
+import Player from './../../interfaces/player';
+
+import { AngularFireList, AngularFireObject, FirebaseOperation } from 'angularfire2/database/interfaces';
+
+import { LadderService } from './../ladder.service';
+
+@Injectable()
+
+export class LadderDatabaseService {
+
+    private _tempPlayers = this._ladder.getPlayers('tekken');
+
+    constructor(private _database: AngularFireDatabase, private _ladder: LadderService) {
+        console.log(this._tempPlayers);
+    }
+
+    public addPlayer(game: string, player: Player) {
+        return this._database.list('ladder/' + game + '/players/').push(player);
+    }
+
+}
