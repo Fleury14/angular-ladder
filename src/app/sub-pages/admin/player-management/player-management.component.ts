15867982146623a21494c380dba05a021b94ff2b
J.R. Ruggiero <fleury14@comcast.net>
2017-12-22 12:54:37 -0800

    tekken ladder initialized, now lets never do that again

--- src/app/sub-pages/admin/player-management/player-management.component.ts
+++ src/app/sub-pages/admin/player-management/player-management.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
 
+import { LadderDatabaseService } from './../../../services/database/ladder-database.service';
+
 @Component({
     selector: 'app-admin-players',
     templateUrl: './player-management.component.html',
     styleUrls: [ './player-management.component.css']
 })
 
-export class PlayerManagementComponent {}
+export class PlayerManagementComponent {
+
+    constructor(private _ladderDB: LadderDatabaseService) {}
+
+    public initialize() {
+        this._ladderDB.instantiation();
+    }
+}
