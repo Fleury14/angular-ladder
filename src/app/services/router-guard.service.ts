5c2e6cd0826f76f4be161cb53d42ebaae87deb7a
J.R. Ruggiero <fleury14@comcast.net>
2017-12-22 21:57:33 -0800

    modified router guard to read from an array of allowed ids. included rado and jard

--- src/app/services/router-guard.service.ts
+++ src/app/services/router-guard.service.ts
@@ -6,13 +6,16 @@ import { LoginService } from './login.service';
 
 export class RouterGuard implements CanActivate {
 
+    private _adminUidList = ['mkomTbU76VUR5tIoLmmyP1luR5q1', 'Gxr9qQ1pczOCLyncfhLiLdhvhy32', 'xFDVJ8Dq9CZR8PmiixbSNX5FpNv2'];
+
+
     constructor (private login: LoginService, private route: Router) {}
 
     canActivate() {
         if (this.login.afAuth.auth.currentUser === null) { // check to see if a user is logged in at all
             this.route.navigate(['home']); // if not, get them out
             return false;
-        } else if (this.login.afAuth.auth.currentUser.providerData[0].uid === '114390552657346311906' ) {
+        } else if (this._adminUidList.includes(this.login.afAuth.auth.currentUser.uid)) {
             // otherwise check to see if its an acceptable admin login
             return true;
         } else { // if not, get them out too
