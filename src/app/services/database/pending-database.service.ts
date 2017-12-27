1c54cca8354b60dc6664abcf965c2bd3c04f6151
J.R. Ruggiero <fleury14@comcast.net>
2017-12-27 12:29:21 -0800

    created dupe check method in service

--- src/app/services/database/pending-database.service.ts
+++ src/app/services/database/pending-database.service.ts
@@ -9,10 +9,27 @@ import { AngularFireList, AngularFireObject, FirebaseOperation } from 'angularfi
 @Injectable()
 
 export class PendingDatabaseService {
-    
-    constructor( private _database: AngularFireDatabase ) {}
 
-    public checkForDupes() {}
+    private _listOfPending;
+
+    constructor( private _database: AngularFireDatabase ) {
+        this._database.list('/w-pending/join').valueChanges().subscribe(pendingList => {
+            this._listOfPending = pendingList;
+        });
+    }
+
+    public checkForDupes(psnId: string) {
+
+        let dupeCheck = false;
+
+        this._listOfPending.forEach(pending => {
+            if (pending.psnId === psnId) {
+                dupeCheck = true;
+            }
+        });
+
+        return dupeCheck;
+    }
 
     public addPending(pending) {
         this._database.list('/w-pending/join').push(pending);
