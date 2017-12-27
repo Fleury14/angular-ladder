c0a0cfe1ffd5117b95addf0accdce9e32d2809c6
J.R. Ruggiero <fleury14@comcast.net>
2017-12-27 11:50:34 -0800

    created pending component and added it to admin routing

---
+++ src/app/sub-pages/admin/pending-management/pending-management.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-admin-pending',
+    templateUrl: './pending-management.component.html',
+    styleUrls: [ './pending-management.component.css']
+})
+
+export class PendingManagementComponent {}
