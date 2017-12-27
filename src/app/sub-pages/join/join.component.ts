5265a0803dbe7211a8c1dcf6d2988690335374b0
J.R. Ruggiero <fleury14@comcast.net>
2017-12-27 12:13:16 -0800

    join component initialization, gets list of games from ladder

--- src/app/sub-pages/join/join.component.ts
+++ src/app/sub-pages/join/join.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
 
+import { LadderDatabaseService } from './../../services/database/ladder-database.service';
+
 @Component({
     selector: 'app-join',
     templateUrl: './join.component.html',
     styleUrls: [ './join.component.css']
 })
 
-export class JoinComponent {}
+export class JoinComponent {
+
+    public gameList; // will contain list of games on the ladder
+    public selectedGame: string // will contain game player clicked on
+
+    constructor( private _ladderDB: LadderDatabaseService) {
+        this._ladderDB.getGameList().subscribe(gameList =>
+        this.gameList = gameList);
+    }
+
+    public selectGame(game: string) {
+        this.selectedGame = game;
+    }
+
+}
