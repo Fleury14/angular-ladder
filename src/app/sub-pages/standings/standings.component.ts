0a6ac34fd64e573f8e53e6680747c8180cd342ad
J.R. Ruggiero <fleury14@comcast.net>
2017-12-27 11:30:17 -0800

    standings now build dynamically off of database

--- src/app/sub-pages/standings/standings.component.ts
+++ src/app/sub-pages/standings/standings.component.ts
@@ -17,11 +17,44 @@ import { LadderDatabaseService } from './../../services/database/ladder-database
 
 export class StandingsComponent {
 
+    public listOfGames;
+    public listOfPlayers = {};
 
     constructor(public masterLadder: LadderService, private _ladderDB: LadderDatabaseService) {
+        this.listOfPlayers = {};
+
+        this._ladderDB.getGameList().subscribe(gameList => {
+            this.listOfGames = gameList;
+            this.listOfGames.forEach(game => {
+                this._ladderDB.getPlayers(game.ref).subscribe(playerList => {
+                    this.listOfPlayers[game.ref] = playerList;
+                });
+            });
+        });
         console.log('Ladder initialized');
-     }
+     } // end constructor
 
+     public sortPlayerList(game, stat, order) {
+        // console.log(`game: ${game} stat: ${stat}`);
+        // console.log(this.listOfPlayers[game][0][stat]);
+        if (order !== 'a' && order !== 'd') {
+            console.log('bad order passed, please pass a or d');
+            return;
+        }
 
+        // first check to make sure property exists on player list
+        if (this.listOfPlayers[game][0][stat] === undefined) {
+            console.log('Bad property passed');
+            return;
+        }
+
+        // check for ascending or descending order
+        if (order === 'a') {
+            this.listOfPlayers[game].sort(function(a, b) { return a[stat] - b[stat]; });
+        } else if (order === 'd') {
+            this.listOfPlayers[game].sort(function(a, b) { return b[stat] - a[stat]; });
+        }
+
+   } // end sortPlayerList
 
 }
