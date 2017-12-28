4305e58b940c74b13fbb66d950df93d1969cfb4b
J.R. Ruggiero <fleury14@comcast.net>
2017-12-28 13:35:32 -0800

    commented pending service

--- src/app/services/database/pending-database.service.ts
+++ src/app/services/database/pending-database.service.ts
@@ -13,16 +13,21 @@ export class PendingDatabaseService {
     private _listOfPending;
     private _MAXPENDING = 30; // maximum number of pending entries
 
+    // instantiate list of pending applications
     constructor( private _database: AngularFireDatabase ) {
         this._database.list('/w-pending/join').valueChanges().subscribe(pendingList => {
             this._listOfPending = pendingList;
         });
     }
 
+    // method to check for duplicate entries
+    // right now, the condition is just matching psnId's within the same game
     public checkForDupes(game: string, psnId: string) {
 
-        let dupeCheck = false;
+        let dupeCheck = false; // reset to false each call
 
+        // go through each item of the list and see if the psn id's and game both match
+        // if so, set the flag to true
         this._listOfPending.forEach(pending => {
             // console.log(`Checking incoming ${psnId} vs iteration ${pending.psnId}`);
             if (pending.psnId === psnId && pending.game === game) {
@@ -33,6 +38,9 @@ export class PendingDatabaseService {
         return dupeCheck;
     }
 
+    // method to make sure the database isnt going over the maximum entries. returns T/F
+    // since basically anyone can now add to the database via the pending apps, we have to put a limit so google doesnt
+    // end up with a million entries and hates me forever
     public pendingFull() {
         if (this._listOfPending >= this._MAXPENDING) {
             return true;
@@ -41,14 +49,17 @@ export class PendingDatabaseService {
         }
     }
 
+    // get a list of pending applicants. mapping is handled on the component level
     public getListOfPending() {
         return this._database.list('/w-pending/').valueChanges();
     }
 
+    // take in an object and add it to the pending database
     public addPending(pending) {
         this._database.list('/w-pending/join').push(pending);
     }
 
+    // take in an id and delete the application with that id from the database
     public deletePending(id) {
         this._database.list('/w-pending/join').remove(id);
     }
