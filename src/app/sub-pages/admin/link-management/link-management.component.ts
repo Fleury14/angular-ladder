d238e9b90ff845e516adde6d03c01093cc8ee1fd
J.R. Ruggiero <fleury14@comcast.net>
2017-12-29 13:31:06 -0800

    created link management component in admin and added it to momdule/routing

---
+++ src/app/sub-pages/admin/link-management/link-management.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-admin-link',
+    templateUrl: './link-management.component.html',
+    styleUrls: [ './link-management.component.css']
+})
+
+export class LinkManagementComponent {}
