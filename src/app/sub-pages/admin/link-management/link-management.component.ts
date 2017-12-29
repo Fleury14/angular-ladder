be40c4cf6eb13fa3c8aeb940e7f4bef87366beee
J.R. Ruggiero <fleury14@comcast.net>
2017-12-29 13:44:23 -0800

    prepared header list and instantiated list of request but i have one more edit on the link pushes

--- src/app/sub-pages/admin/link-management/link-management.component.ts
+++ src/app/sub-pages/admin/link-management/link-management.component.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
 
+import { PendingDatabaseService } from './../../../services/database/pending-database.service';
+import { LadderDatabaseService } from './../../../services/database/ladder-database.service';
+
 @Component({
     selector: 'app-admin-link',
     templateUrl: './link-management.component.html',
     styleUrls: [ './link-management.component.css']
 })
 
-export class LinkManagementComponent {}
+export class LinkManagementComponent {
+
+    public listOfPendingLinks; // will contain list of pending links
+
+    constructor(private _ladderDB: LadderDatabaseService, private _pending: PendingDatabaseService) {
+        this._pending.getListOfPending().map(gameList => {
+            // use dunlavy RR tech to put key in object
+            const pendingList = [];
+            for (const pendKey in gameList[1]) {
+                const pendLoop = gameList[1][pendKey];
+                pendLoop.id = pendKey;
+                pendingList.push(pendLoop);
+            }
+            return pendingList;
+        })
+        .subscribe(gameList => {
+            this.listOfPendingLinks = gameList;
+            console.log(this.listOfPendingLinks);
+        });
+    }
+
+}
