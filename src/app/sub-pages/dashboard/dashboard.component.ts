2d0bd906ca0db9aa3408d51dc6c7de80c074cb69
J.R. Ruggiero <fleury14@comcast.net>
2017-12-29 10:45:03 -0800

    dashboard displays username

--- src/app/sub-pages/dashboard/dashboard.component.ts
+++ src/app/sub-pages/dashboard/dashboard.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
 
+import { LoginService } from './../../services/login.service';
+import { LadderDatabaseService } from './../../services/database/ladder-database.service';
+
 @Component({
     selector: 'app-dashboard',
     templateUrl: './dashboard.component.html',
     styleUrls: [ './dashboard.component.css']
 })
 
-export class DashboardComponent {}
+export class DashboardComponent {
+
+    constructor(public login: LoginService, private _ladderDB: LadderDatabaseService) {
+
+    }
+
+}
