5ea107aebd70a6f5fa878888fb7338a03ce5f3da
J.R. Ruggiero <fleury14@comcast.net>
2017-12-30 12:08:43 -0800

    added some navigation validation and a start over button

--- src/app/sub-pages/place-challenge/place-challenge.component.ts
+++ src/app/sub-pages/place-challenge/place-challenge.component.ts
@@ -19,6 +19,7 @@ export class PlaceChallengeComponent {
     private _user; // user login info
 
     // progression flags
+    public canSelectGame = true;
     public canSelectPlayer = false;
     public canSelectDefender = false;
     public linkNoPlayerWarning = false;
@@ -65,13 +66,14 @@ export class PlaceChallengeComponent {
 
                 if (this.selectedChallenger) {
                     // only allow passage to next part if they picked a game that found a player
+                    // console.log('found player:', this.selectedChallenger);
                     this.linkNoPlayerWarning = false;
+                    this.canSelectGame = false;
                     this.canSelectDefender = true;
                 } else {
                     // otherwise display the warning
                     this.linkNoPlayerWarning = true;
                 }
-                // console.log('found player:', this.selectedChallenger);
 
             }
         });
@@ -84,7 +86,35 @@ export class PlaceChallengeComponent {
         // cancel select player if they have a linked google account
         if (player.google) {return; }
         this.selectedChallenger = player;
+        this.canSelectGame = false;
         this.canSelectDefender = true;
         this.canSelectPlayer = false;
+    }
+
+    // method to make sure challenges are valid
+    public challengeValidation(challenger, defender) {
+        // default to true
+        let result = true;
+
+        // make sure rank difference is greater than 0 (i.e. theyre not challenging someone below them)
+        // also make sure that the rank difference is less than 4 (i.e. theyre not challenging someone too high for them)
+        const rankDiff = challenger.rank - defender.rank;
+        if (rankDiff < 1 || rankDiff > 3) {result = false; }
+
+        // see if the defender has a recent opponent id. if so, make sure its not matching the challnger
+        if (defender.recentId) {
+            if (defender.recentId === challenger.id) {result = false; }
+        }
+
+        return result;
+
+    } // end challenge validation
+
+    public startOver() {
+        this.canSelectGame = true;
+        this.canSelectPlayer = false;
+        this.canSelectDefender = false;
+        this.linkNoPlayerWarning = false;
+        this.challengeMethod = 0; // will contain the user selection in how to challenge someone. 1 = linked 2 = anon
     }
 }
