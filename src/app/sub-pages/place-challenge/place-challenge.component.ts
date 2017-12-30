6bf2c7bb6845407ff6f0ab7f907f87e299469aaa
J.R. Ruggiero <fleury14@comcast.net>
2017-12-30 10:47:51 -0800

    user can select game and player list is instantiated

--- src/app/sub-pages/place-challenge/place-challenge.component.ts
+++ src/app/sub-pages/place-challenge/place-challenge.component.ts
@@ -12,6 +12,11 @@ export class PlaceChallengeComponent {
 
     public challengeMethod = 0; // will contain the user selection in how to challenge someone. 1 = linked 2 = anon
     public listOfGames; // will contain list of games
+    public selectedGame: string; // will contain the game the user selects
+    public listofPlayers; // will contain a list of players for selected game
+
+    // progression flags
+    public canSelectPlayer = false;
 
     constructor(private _ladderDB: LadderDatabaseService) {
         this._ladderDB.getGameList().subscribe(gameList => {
@@ -26,5 +31,16 @@ export class PlaceChallengeComponent {
 
     public useAnon() {
         this.challengeMethod = 2;
+    }
+
+    public selectGame(game: string) {
+        // set selected game and instantiate player list
+        this.selectedGame = game;
+        this._ladderDB.getPlayers(game).subscribe(playerList => {
+            this.listofPlayers = playerList;
+        });
+
+        // progress to next section
+        this.canSelectPlayer = true;
     }
 }
