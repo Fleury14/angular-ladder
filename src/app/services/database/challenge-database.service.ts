29d3e2f9c03b5775d02cfc7f703c209e8d5d60ed
J.R. Ruggiero <fleury14@comcast.net>
2017-12-30 13:38:24 -0800

    created add and delete methods in challenge service

--- src/app/services/database/challenge-database.service.ts
+++ src/app/services/database/challenge-database.service.ts
@@ -6,10 +6,19 @@ import { AngularFireDatabase } from 'angularfire2/database';
 import NewsItem from './../../interfaces/news-item';
 import { AngularFireList, AngularFireObject, FirebaseOperation } from 'angularfire2/database/interfaces';
 
+import ChallengeDB from './../../interfaces/challenge-db';
+
 @Injectable()
 
 export class ChallengeDatabaseService {
 
     constructor (private _database: AngularFireDatabase) {}
 
+    public addChallenge(challenge: ChallengeDB) {
+        this._database.list('/x-challenges').push(challenge);
+    }
+
+    public deleteChallenge(id) {
+        this._database.list('/x-challenges').remove(id);
+    }
 }
