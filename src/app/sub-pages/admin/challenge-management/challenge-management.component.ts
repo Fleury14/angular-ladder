727071a2611603355032a8d7ee8347af760d0f46
J.R. Ruggiero <fleury14@comcast.net>
2017-12-31 12:34:55 -0800

    commented challenege-management

--- src/app/sub-pages/admin/challenge-management/challenge-management.component.ts
+++ src/app/sub-pages/admin/challenge-management/challenge-management.component.ts
@@ -11,30 +11,33 @@ import { ChallengeDatabaseService } from './../../../services/database/challenge
 
 export class ChallengeManagementComponent {
 
-    public listOfPendingChallenges;
-    public listOfActiveChallenges;
+    public listOfPendingChallenges; // will contain a list of anon chasllenges that are pending
+    public listOfActiveChallenges; // will conatin a list of active challenges
 
     constructor (private _pending: PendingDatabaseService, private _challengeDB: ChallengeDatabaseService) {
         this._pending.getListOfPendingChallenges().subscribe(pendingList => {
             this.listOfPendingChallenges = pendingList;
-            console.log('list of pendings:', this.listOfPendingChallenges);
+            // onsole.log('list of pendings:', this.listOfPendingChallenges);
         });
 
         this._challengeDB.getListOfChallenges().subscribe(challengeList => {
             this.listOfActiveChallenges = challengeList;
-            console.log('list of actives:', this.listOfActiveChallenges);
+            // console.log('list of actives:', this.listOfActiveChallenges);
         });
     }
 
+    // method to deny challenge. deletes it from the pending list
     public denyChallenge(id) {
         this._pending.deletePendingChallenge(id);
     }
 
+    // method to approve challenge. deletes it from the pending list and adds to active challenges
     public approveChallenge(challenge, id) {
         if (confirm('Confirm adding selected challenge to official challenge list')) {
+            // add 10 days in ubnix time to the current date to pass correct deadling
             challenge['deadline'] = Date.now() + 864000000;
             this._pending.deletePendingChallenge(id);
-            challenge.id = null;
+            challenge.id = null; // remove the challenge id from the pending list so that its not confused with the id in the challenge list
             console.log('submitting challenge:', challenge);
             this._challengeDB.addChallenge(challenge);
 
@@ -42,6 +45,7 @@ export class ChallengeManagementComponent {
 
     }
 
+    // method to delete an active challenge from the database
     public deleteActiveChallenge(id) {
         if (confirm('Are you sure you want to delete this challenge?')) {
             this._challengeDB.deleteChallenge(id);
