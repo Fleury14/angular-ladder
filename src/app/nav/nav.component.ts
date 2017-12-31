963fd933c738fb233efdddab476f566fd7d4913f
J.R. Ruggiero <fleury14@comcast.net>
2017-12-31 11:36:49 -0800

    user dashboard now displays a badge with the number of challenges a player is involved in

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 import { LoginService } from './../services/login.service';
 import { LadderDatabaseService } from './../services/database/ladder-database.service';
+import { ChallengeDatabaseService } from './../services/database/challenge-database.service';
 
 @Component({
     selector: 'app-nav',
@@ -14,12 +15,28 @@ export class NavComponent {
     private _adminList = ['114390552657346311906'];
     // in order: jr, rado
     private _adminUidList = ['mkomTbU76VUR5tIoLmmyP1luR5q1', 'Gxr9qQ1pczOCLyncfhLiLdhvhy32', 'xFDVJ8Dq9CZR8PmiixbSNX5FpNv2'];
-    public listOfGames;
-    private _loginInfo;
+    // public listOfChallenges;
     public challengeBadge: number;
+    private _loginInfo;
 
 
-    constructor(public login: LoginService, private _ladderDB: LadderDatabaseService) {
+    constructor(public login: LoginService, private _challengeDB: ChallengeDatabaseService) {
+
+        this.login.getLoggedInUserObs().subscribe(info => {
+            this._loginInfo = info;
+            this._challengeDB.getListOfChallenges().subscribe(challengeList => {
+                this.challengeBadge = 0;
+                challengeList.forEach(challenge => {
+                if (challenge['challengerGoogle'] === this._loginInfo.uid) {
+                    this.challengeBadge++;
+                }
+                if (challenge['defenderGoogle'] === this._loginInfo.uid) {
+                    this.challengeBadge++;
+                }
+                });
+            });
+        }); 
+
     }
 
     public adminCheck(uid: string) {
