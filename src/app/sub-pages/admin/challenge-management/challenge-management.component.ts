18a02af0182076cc0ad849aac19908d8ededb97d
J.R. Ruggiero <fleury14@comcast.net>
2017-12-30 16:30:20 -0800

    list of pending challenges now displays on admin screen

--- src/app/sub-pages/admin/challenge-management/challenge-management.component.ts
+++ src/app/sub-pages/admin/challenge-management/challenge-management.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
 
+import { PendingDatabaseService } from './../../../services/database/pending-database.service';
+import { ChallengeDatabaseService } from './../../../services/database/challenge-database.service';
+
 @Component({
     selector: 'app-admin-news',
     templateUrl: './challenge-management.component.html',
     styleUrls: [ './challenge-management.component.css' ]
 })
 
-export class ChallengeManagementComponent {}
+export class ChallengeManagementComponent {
+
+    public listOfPendingChallenges;
+
+    constructor (private _pending: PendingDatabaseService, private _challengeDB: ChallengeDatabaseService) {
+        this._pending.getListOfPendingChallenges().subscribe(pendingList => {
+            this.listOfPendingChallenges = pendingList;
+        });
+    }
+
+}
