747d04c412f0f49f98ad1fc1552f122e6d25b455
J.R. Ruggiero <fleury14@comcast.net>
2018-01-01 11:03:37 -0800

    front end of player adjustment is done, just need to figure out how to push those updates to the db.. set or update?

--- src/app/sub-pages/admin/challenge-management/challenge-management.component.ts
+++ src/app/sub-pages/admin/challenge-management/challenge-management.component.ts
@@ -69,8 +69,11 @@ export class ChallengeManagementComponent {
     }
 
     public approveResult(result) {
-        this.listOfAffectedPlayers = [];
-        this._currentDefender = null;
+        this.listOfAffectedPlayers = []; // list of all players whos rank will be lowered
+        this._currentDefender = null; // the current defender.
+        // NOTE: The reason the current defender is seperate is because its possible that the defender has a higher than 3 rank difference
+        // if they won their own challenge. as a result, any adjustments to rank to the defender only will be done in this object.
+        // when it comes time to update, we will make sure that the same person does not get pushed twice
         this._ladderDB.getPlayers(result.game).subscribe(playerList => {
             this._currentDefender = playerList.find(function(e, i, a) {
                 if (e.id === result.defenderId) { return true; }
@@ -96,11 +99,78 @@ export class ChallengeManagementComponent {
 
     // method to make player adjustments based off a challenger win
     private _winAdjust(result) {
-        const lastIndex = this.listOfAffectedPlayers.length();
-    }
+        // the last player in the list of affected players should always be the challenger.
+        const chall = this.listOfAffectedPlayers.length() - 1;
+
+        // increment all affected players rank by 1
+        this.listOfAffectedPlayers.forEach(player => { player.rank++; });
+        // then readjust challengers rank based on the challenge data
+        this.listOfAffectedPlayers[chall].rank = result.defenderRank;
+
+        // adjust wins and losses
+        this._currentDefender.losses++;
+        this.listOfAffectedPlayers[chall].wins++;
+
+        // adjust ELO
+        const challELO = this.listOfAffectedPlayers[chall].elo;
+        const defELO = this._currentDefender.elo;
+        this.listOfAffectedPlayers[chall].elo = this._getNewRating(challELO, defELO, 1);
+        this._currentDefender.elo = this._getNewRating(defELO, challELO, 0);
+
+        // make sure defender isnt included in both affectedPlayers and currentDefender
+        // if current defender is actually in the afectedPlayers list, he should always be first so a simple shift() should work
+        if (this._currentDefender.id === this.listOfAffectedPlayers[0].id) {
+            this.listOfAffectedPlayers.shift();
+        }
+
+        // finally, push adjustments to db
+        this.listOfAffectedPlayers.forEach(player => {
+            // update affected players
+        });
+
+        // update defender
+}
 
     // method to make player adjustments based off a defender win
-    private _lossAdjust(result) {}
+    private _lossAdjust(result) {
+        // the last player in the list of affected players should always be the challenger.
+        // in the event of a defender win, that's all we're interested in since no ranks will be moved
+        const chall = this.listOfAffectedPlayers.length() - 1;
+
+        // add wins and losses
+        this._currentDefender.wins++;
+        this.listOfAffectedPlayers[chall].losses++;
+
+        // adjust elo
+        const challELO = this.listOfAffectedPlayers[chall].elo;
+        const defELO = this._currentDefender.elo;
+        this.listOfAffectedPlayers[chall].elo = this._getNewRating(challELO, defELO, 0);
+        this._currentDefender.elo = this._getNewRating(defELO, challELO, 1);
+
+        // if the challenger rank is #2, force them to wait a couple days to place another challenge so that they can't
+        // constantly have a lock on the title shot. we'll give them 3 days, 259200000 seconds
+        if (this.listOfAffectedPlayers[chall].rank === 2) {
+            this.listOfAffectedPlayers.champWait = Date.now() + 259200000;
+        }
+
+        // update defender
+
+        // update challenger
+    }
+
+    // ELO functions from github: moroshko
+    private _getRatingDelta(myRating, opponentRating, myGameResult) {
+        if ([0, 0.5, 1].indexOf(myGameResult) === -1) {
+          return null;
+        }
+
+        const myChanceToWin = 1 / ( 1 + Math.pow(10, (opponentRating - myRating) / 400));
+
+        return Math.round(32 * (myGameResult - myChanceToWin));
+    }
+    private _getNewRating(myRating, opponentRating, myGameResult) {
+        return myRating + this._getRatingDelta(myRating, opponentRating, myGameResult);
+    }
 
     public unixConvert(unix: number): Date {
         return new Date(unix);
