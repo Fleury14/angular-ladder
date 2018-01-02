4449ebaddc7fd156f75a024643298cde6d2ef9fe
J.R. Ruggiero <fleury14@comcast.net>
2018-01-01 17:33:21 -0800

    created matchhistory management component in admin and added to routing

---
+++ src/app/sub-pages/admin/match-history-management/match-history-management.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+import { MatchHistoryDatabaseService } from './../../../services/database/match-history-database.service';
+
+@Component({
+    selector: 'app-admin-match-history',
+    templateUrl: './match-history-management.html',
+    styleUrls: [ './match-history-management.css']
+})
+
+export class MatchHistoryManagementComponent {}
