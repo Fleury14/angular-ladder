ef859feccd6da1c93472500dd8548613fc18c88f
J.R. Ruggiero <fleury14@comcast.net>
2018-01-03 13:39:26 -0800

    commented ts and added a hover effect to the burger

--- src/app/sub-pages/admin/admin.component.ts
+++ src/app/sub-pages/admin/admin.component.ts
@@ -9,10 +9,11 @@ import { Router } from '@angular/router';
 
 export class AdminComponent implements OnInit {
 
-    public hideMenu = true;
+    public hideMenu = true; // flag that determins if menu is hidden
 
     constructor(private _route: Router) {}
 
+    // method to toggle hideMenu to true or false on a click
     public toggleMenu() {
         console.log('burger triggered');
         if (this.hideMenu === true) {
@@ -23,7 +24,10 @@ export class AdminComponent implements OnInit {
     }
 
     ngOnInit() {
+        // subscribe to the router events...
         this._route.events.subscribe(routeChange => {
+            // ..so that this code to hide the menu runs every time a route is changed. this way,
+            // when someone clicks on a link in the menu, the menu hides automatically
             this.hideMenu = true;
         });
     }
