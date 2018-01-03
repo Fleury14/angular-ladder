bd8c05f0fa8966d9f629114205feda3638bbfa4e
J.R. Ruggiero <fleury14@comcast.net>
2018-01-03 13:17:28 -0800

    changed row of buttons to an off canvas side nav

--- src/app/sub-pages/admin/admin.component.ts
+++ src/app/sub-pages/admin/admin.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-admin',
@@ -6,6 +7,24 @@ import { Component } from '@angular/core';
     styleUrls: ['./admin.component.css']
 })
 
-export class AdminComponent {
+export class AdminComponent implements OnInit {
 
+    public hideMenu = true;
+
+    constructor(private _route: Router) {}
+
+    public toggleMenu() {
+        console.log('burger triggered');
+        if (this.hideMenu === true) {
+            this.hideMenu = false;
+        } else {
+            this.hideMenu = true;
+        }
+    }
+
+    ngOnInit() {
+        this._route.events.subscribe(routeChange => {
+            this.hideMenu = true;
+        });
+    }
 }
