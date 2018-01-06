b7da474b3bde476d2b6751afe820b79070226d1d
J.R. Ruggiero <fleury14@comcast.net>
2018-01-06 12:06:02 -0800

    instantiated list... and did some formatting

--- src/app/sub-pages/admin/admin-home/admin-home.component.ts
+++ src/app/sub-pages/admin/admin-home/admin-home.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 
+import { PendingDatabaseService } from './../../../services/database/pending-database.service';
+
 @Component({
     selector: 'app-admin-home',
     templateUrl: './admin-home.component.html',
     styleUrls: [ './admin-home.component.css' ]
 })
 
-export class AdminHomeComponent {}
+export class AdminHomeComponent {
+    public numOfJoins = 0;
+
+    constructor(private _pendingDB: PendingDatabaseService) {
+        this._pendingDB.getListOfPending().subscribe(list => {
+            this.numOfJoins = list.length;
+        });
+    }
+}
