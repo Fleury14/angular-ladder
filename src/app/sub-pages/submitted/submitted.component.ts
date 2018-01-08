f19ce3bb37c4b4c574e1149624c48c13a26f5b1d
J.R. Ruggiero <fleury14@comcast.net>
2018-01-08 11:10:12 -0800

    created submit component

---
+++ src/app/sub-pages/submitted/submitted.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+    selector: 'app-submit',
+    templateUrl: './submitted.component.html',
+    styleUrls: [ './submitted.component.css']
+})
+
+export class SubmitComponent {}
