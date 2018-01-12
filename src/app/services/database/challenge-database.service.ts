d9ac729905e9513f61a5db162479b9ecc674850e
J.R. Ruggiero <fleury14@comcast.net>
2018-01-12 12:02:11 -0800

    commented challenge db service

--- src/app/services/database/challenge-database.service.ts
+++ src/app/services/database/challenge-database.service.ts
@@ -26,16 +26,26 @@ export class ChallengeDatabaseService {
         this._database.list('/x-challenges').push(challenge);
     }
 
+    // method used when updating the ranks on the challenge list to match the ranks on the ladder
+    // not using this will cause a problem when players move around due to other challenges but their rank
+    // on the challenge stays the same. for example if #5 challenged #4 then at the point of the challenge
+    // those are the ranks eneter in. but lets say someone below them jumped them both and now they are #5 and #6
+    // the challenge ranks are not updated and when the score goes to post, bad things happen. this resolves that problem
     public matchChallengeRank() {
+        // iterate through each challenge
         this._challengeList.forEach(challenge => {
-            const foundChallenge = [];
+            // grab a list of players and subscribe to it. assign it to a var so we can unsub later
             const playerSub = this._ladderDB.getPlayers(challenge.game).subscribe(playerList => {
+                // iterate through each player on the list looking for a matching id
                 playerList.forEach(player => {
+                    // if the iterated player id matches the challenger id then we have a match, so we need to make sure the ranks are good
                     if (player.id === challenge.challengerId) {
                         // console.log('Match found - Challenger');
                         if (player.rank === challenge.challengerRank) {
+                            // if the players rank on the ladder and rank on the challenge match up, then we good...
                             // console.log('Ranks match');
                         } else {
+                            // but if they DONT, edit the appropriate rank and update the challenge
                             // console.log('Ranks do NOT match');
                             // console.log(`${challenge.challengerName}'s rank is ${challenge.challengerRank} but is ${player.rank} on the ladder.`);
                             challenge.challengerRank = player.rank;
@@ -44,6 +54,7 @@ export class ChallengeDatabaseService {
                         }
                     }
                     if (player.id === challenge.defenderId) {
+                        // same thing for the defender id
                         // console.log('Match found - Defender');
                         if (player.rank === challenge.defenderRank) {
                             // console.log('Ranks match');
@@ -56,6 +67,7 @@ export class ChallengeDatabaseService {
                         }
                     }
                 });
+                // unsub when we're done because we dont want this running everytime theres a database edit
                 playerSub.unsubscribe();
             });
         });
