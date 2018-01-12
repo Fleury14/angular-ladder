e7363ee3f55bf9a3e1e8689efc068e787795a6c3
J.R. Ruggiero <fleury14@comcast.net>
2018-01-11 19:56:28 -0800

    wrote backend function to retrieve list of challenges as an api call

--- functions/index.js
+++ functions/index.js
@@ -5,12 +5,31 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 // player check firebase call?
-exports.playerCheck = functions.https.onRequest((req, res) => {
-    // const player2BeChecked = req.body.player;
-    // const game = req.body.game;
-    const playerList = functions.database.ref('/ladder/' + 'tekken' + '/players/');
-    console.log(playerList);
-    res.send('testing', playerList);
+exports.getChallenges = functions.https.onRequest((req, res) => {
+    
+    const query = admin.database().ref('/x-challenges');
+    const challenges = [];
+    let test = '';
+    query.once('value').then(snapshot => {
+        snapshot.forEach(childSnap => {
+            console.log('Child Snap:', childSnap.val());
+            challenges.push({
+                key: childSnap.key,
+                challenger: childSnap.val().challengerName,
+                defender: childSnap.val().defenderName,
+                deadline: new Date(childSnap.val().deadline)
+            });
+        })
+        res.send(challenges);
+    });
+
+    
+    
+    // const challengeRef = db.ref('/x-challenges');
+    // challengeRef.once('value', function(snapshot) {
+    //     res.send(snapshot.val().json());
+    // })
+    // // res.send('This is where i will have a list of challenges');
 
     
 })
