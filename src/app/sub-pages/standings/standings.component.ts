1b1c3ebd0f78a4e04e89bf1693392aef3abd8ff0
J.R. Ruggiero <fleury14@comcast.net>
2018-01-13 12:04:06 -0800

    modified sort method to account for game w/l%

--- src/app/sub-pages/standings/standings.component.ts
+++ src/app/sub-pages/standings/standings.component.ts
@@ -43,13 +43,35 @@ export class StandingsComponent {
             console.log('bad order passed, please pass a or d');
             return;
         }
+        // check to see if stat is game pct as thats not a property but is an exception
+        // otherwise make sure property exists on player list
+        if (stat === 'gamepct') {
+            console.log('sorting by game %');
+            switch (order) {
+                case 'a':
+                    this.listOfPlayers[game].sort(function(a, b) {
+                        if (a.gameWins === 0) { return -1; } else if (b.gameWins === 0) { return 1; } else {
+                            return (a.gameWins / (a.gameWins + a.gameLosses)) - (b.gameWins / (b.gameWins + b.gameLosses));
+                        }
+                    });
+                    break;
 
-        // first check to make sure property exists on player list
-        if (this.listOfPlayers[game][0][stat] === undefined) {
+                case 'd':
+                this.listOfPlayers[game].sort(function(a, b) {
+                    if (b.gameWins === 0) { return -1; } else if (a.gameWins === 0) { return 1; } else {
+                        return (b.gameWins / (b.gameWins + b.gameLosses)) - (a.gameWins / (a.gameWins + a.gameLosses));
+                    }
+                });
+                    break;
+            }
+            return;
+
+        } else if (this.listOfPlayers[game][0][stat] === undefined) {
             console.log('Bad property passed');
             return;
         }
 
+        console.log(`sorting by ${stat}`);
         // check for ascending or descending order
         if (order === 'a') {
             this.listOfPlayers[game].sort(function(a, b) { return a[stat] - b[stat]; });
